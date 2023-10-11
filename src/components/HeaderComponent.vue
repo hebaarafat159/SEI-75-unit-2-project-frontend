7b5bd5dcfa6687ca7b38d8e2b3af6d996c2e7564
hebaarafat159 <heba.arafat159@gmail.com>
2023-10-11 23:58:37 +0100

    show user shopping list in header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,7 +5,17 @@
         </div>
         <div class="cart" id="cart_id" @click="openCartPage">
             <div><img src="../assets/cart_icon.png" alt="" id="cartIcon"></div>
-            <div id="cartName">cart</div> 
+
+            <div v-if="isLoggedIn">
+                <select name="list" id="list" v-model="selectedValue">
+                    <option :value="null" disabled> Select a category</option>
+                    <option v-for="list in userShoppingLists" :key="list.id" 
+                    :value="list"> {{ list.name }}</option>
+                </select> 
+            </div>
+            <div v-else>
+                <div id="cartName">cart</div> 
+            </div>
         </div>
         <div>
             <div v-if="isLoggedIn">
@@ -28,17 +38,17 @@ export default {
     data: () => ({
         isInit: false,
         isLoggedIn: false,
-        userObject:{},
-        currentListId:'',
-        userShoppingLists:[]
+        userObject:null,
+        currentList:null,
+        userShoppingLists:[],
+        selectedValue: ''
     }),
     mounted(){
         if(this.$cookies.isKey('user_session')){
             this.isLoggedIn = true
-            const userData = decodeCredential(this.$cookies.get('user_session'))
-            this.userObject = userData; 
-            if(userData!== null){
-                console.log(`${userData.email} is logged in ..... `);
+            this.userObject = decodeCredential(this.$cookies.get('user_session'))
+            if(this.userObject!== null){
+                console.log(`${this.userObject.email} is logged in ..... `);
 
             }else{
                 console.log(`please login ..... `);
@@ -51,10 +61,7 @@ export default {
         },
         callback: function (response) {
             this.isLoggedIn = true
-            let userData = decodeCredential(response.credential)
-            // TODO delete this line
-            console.log(userData);
-            this.userObject = userData;
+            this.userObject  = decodeCredential(response.credential)
             console.log(`Google User: ${JSON.stringify(this.userObject)}`);
             this.$cookies.set('user_session', response.credential);
             fetch("http://localhost:4000/users/login",{
@@ -69,8 +76,7 @@ export default {
                   console.log(res.status)
                   if(res.status === 200){
                     console.log('Saved Successfully');
-                    // this.$router.push({name: 'All Categories'});
-                    this.getUsersList()
+                    this.getUsersList();
                   }
               }).catch (error => {
                 console.log(error);
@@ -82,9 +88,10 @@ export default {
             this.isLoggedIn = false
         },
         openCartPage: function(){
-            // TODO remove this line
-            this.currentListId = '6523ec21549acd91ef6ac71a'; 
-            this.$router.replace({path: `/shoppingLists/${this.currentListId}/listItems`});
+            if(this.currentList !== null)
+            {
+                this.$router.replace({path: `/shoppingLists/${this.currentList._id}/listItems`});
+            }
         },
         getUsersList: function(){
             this.userObject.emial = 'heba.arafat159@gmail.com'
@@ -93,6 +100,11 @@ export default {
             .then(result => {
                     console.log(`User Lists: ${JSON.stringify(result.body)}`);
                     this.userShoppingLists = result.body;
+                    this.userShoppingLists.forEach(element => {
+                        if(element.isSelected)
+                            this.currentList = element;
+                            this.selectedValue = this.currentList.name;
+                    });
             })
         }
     }
