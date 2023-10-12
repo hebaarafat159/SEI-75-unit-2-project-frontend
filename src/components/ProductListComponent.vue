9c9ff149dcc61efab6cf54284aead6aeeacfd150
hebaarafat159 <heba.arafat159@gmail.com>
2023-10-12 13:44:26 +0100

    fix select single item list issue

--- src/components/ProductListComponent.vue
+++ src/components/ProductListComponent.vue
@@ -4,11 +4,11 @@
             <div><img src="../assets/cart_icon.png" alt="" id="proImage"></div>
             <div class="productContent">
                 <h2> {{ product.name }} </h2>
-                <div v-if="isAdded">
+                <div v-if="selectedId === product._id">
                    <AddProductComponent :product="product"></AddProductComponent> 
                 </div>
                 <div v-else>
-                    <button class="btn btn-info" v-on:click="addProductToList" id="addIcon"> Add </button>
+                    <button class="btn btn-info" v-on:click="addProductToList" id="addIcon" :value="product._id"> Add </button>
                     <!-- <img src="../assets/add_icon.png" alt="" v-on:click="addProductToList" id="addIcon"> -->
                 </div>
             </div>
@@ -23,17 +23,20 @@
         data: () => ({
             isInit: false,
             isLoggedIn: false,
-            isAdded: false
+            isAdded: false,
+            selectedId:''
         }),
         props : {
              productsList: Array,
          },
          methods:{
-            addProductToList: function(){
-                console.log('Adding Prduct to list');
-                // TODO hide add button
-                this.isAdded = true;
-                // TODO show adding component view
+            addProductToList: function(event){
+                console.log(`Adding Prduct to list ::: ${event.target.value}`);
+                this.selectedId = event.target.value;
+                // if(event.target.value === )
+                // // TODO hide add button
+                // this.isAdded = true;
+                // // TODO show adding component view
                 // TODO get data from component 
                 // TODO send put request tp backend
             }
