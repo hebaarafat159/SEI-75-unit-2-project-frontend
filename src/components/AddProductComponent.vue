52051e01577635b6f6485dccd5421e264b97a396
hebaarafat159 <heba.arafat159@gmail.com>
2023-10-13 13:38:10 +0100

    fix add and remove list item issue

--- src/components/AddProductComponent.vue
+++ src/components/AddProductComponent.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="productAddView" v-bind="product" :key="product._id">
+    <div class="productAddView" v-bind="item.productObj" :key="product._id">
         <div>
-            <p><input type="text" name="quantity" v-model="productItemList.quantity" placeholder="0.0"></p> 
-            <select name="list" id="list" v-model="selectedValue">    
+            <p><input type="text" name="quantity" v-model="item.quantity" placeholder="0.0"></p> 
+            <select name="item.selectedMeasure" id="item.selectedMeasure" v-model="item.selectedMeasure">    
                 <option :value="null" disabled> Select a measure</option>
-                <option v-for="list in productItemList.product.measures" :key="list" :value="list"> 
-                    {{ list }}
+                <option v-for="measure in item.productObj.measures" :key="measure._id" :value="measure" > 
+                    {{ measure.name }}
                 </option>
             </select>
         </div>
@@ -21,59 +21,41 @@
         name: 'AddProductComponent',
         data:() => ({
             quantityText:'',
-            productItemList:{
-                item_id:'6526fc20ab61522c28daef25',
-                product_id: '6521bb2ff39360ef56c544f2',
-                quantity:43,
-                measure_id:'6521bc7cf39360ef56c544f6',
-                lastUpdatedDate: new Date().getTime(),
-                hasBrought:false,
-                list_id:'6523ec21549acd91ef6ac71a',
-                measure: {
-                    _id:"6521bd32f39360ef56c544fb",
-                    name:"Package"
-                },
-                product: {
-                    _id:"6521b600f39360ef56c544e5",
-                    name:"Tomato",
-                    image:"",
-                    category_id:"6521b262f39360ef56c544e2",
-                    description:"",
-                    measures:["6521ef9ff39360ef56c54501"]
-                }
-                // item_id:'',
-                // product_id: this.product.id,
-                // quantity: this.quantityText,
-                // measure_id: this.product.measures[0],
-                // lastUpdatedDate: new Date().getTime(),
-                // hasBrought:false,
-                // list_id:'6523ec21549acd91ef6ac71a'
+            item:{
+                _id:'-1',
+                productObj: {},
+                quantity: 0,
+                selectedMeasure: {},
+                status:false,
+                list:{
+                    _id : '6523ec21549acd91ef6ac71a'
+                }, // TODO add list object
             }
         }),
+        mounted() {
+            this.item.productObj = this.product;
+            this.selectedMeasure = this.item.productObj.measures[0];
+        },
         props : {
-            quantity: Number,
-            product: Object,
-            measure: Object
-         },
-         methods:{
+             product: Object,
+        },
+        methods:{
             saveProduct: function(){
-                // console.log(`Saving Prduct to list :::::::: ${JSON.stringify(this.productItemList)}`);
-                fetch(`http://localhost:4000/shoppingLists/${this.productItemList.list_id}/listItems/add`,{
+                console.log(`Saving Prduct to list :::::::: ${JSON.stringify(this.item)}`);
+                fetch(`http://localhost:4000/shoppingLists/${this.item.list._id}/listItems/add`,{
                     method: "POST",
                     headers:{
                         "Content-Type" : "application/json"
                     },
       
-                    body: JSON.stringify(this.productItemList)
+                    body: JSON.stringify(this.item)
                 })
                 .then(response => response.json())
                 .then(result => {
-                    // console.log(`Saved Or Update Successfully : ${JSON.stringify(result)}` );
-                    // // console.log(res.status)
                     if(result.status === 200){
-                        console.log(`Saved Successfully : ${JSON.stringify(result.body)}` );
-                        console.log(`New Item ID : ${JSON.stringify(result.body._id)}` );
-                        this.productItemList.item_id = result.body._id;
+                        console.log(`Saved Or Update Successfully : ${JSON.stringify(result.body)}` );
+                        console.log(`New Item ID : ${JSON.stringify(result.body._id)}`);
+                        this.item._id = result.body._id;
                         // this.$router.push({name: 'All Books'});
                     }
                 })  
@@ -82,13 +64,13 @@
                 })
             },
             removeProduct: function(){
-                console.log(`Remove Prduct to list :::::::: ${JSON.stringify(this.productItemList)}`);
-                fetch(`http://localhost:4000/shoppingLists/${this.productItemList.list_id}/listItems/delete`,{
+                console.log(`Remove Prduct to list :::::::: ${JSON.stringify(this.item)}`);
+                fetch(`http://localhost:4000/shoppingLists/${this.item.list._id}/listItems/delete`,{
                     method: "DELETE",
                     headers:{
                         "Content-Type" : "application/json"
                     },
-                    body: JSON.stringify(this.productItemList)
+                    body: JSON.stringify(this.item)
                 })
                 .then(response => response.json())
                 .then(result => {
